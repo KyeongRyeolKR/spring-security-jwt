860e47dc8eaa2aa9108ad1fb47eac1db6e2e024a
김경렬 <rlarudfuf802@naver.com>
2024-03-21 19:10:58 +0900

    feat: CORS 설정

--- src/main/java/com/example/securityjwt/config/SecurityConfig.java
+++ src/main/java/com/example/securityjwt/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.securityjwt.config;
 import com.example.securityjwt.jwt.JwtFilter;
 import com.example.securityjwt.jwt.JwtUtil;
 import com.example.securityjwt.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +32,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // CORS 설정 - 웹 브라우저에서 출처가 다른 곳에서 자원을 공유하는걸 허락하기 위한 설정
+                .cors(
+                        (cors) -> cors
+                                .configurationSource(new CorsConfigurationSource() {
+                                    @Override
+                                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                        CorsConfiguration config = new CorsConfiguration();
+                                        config.setAllowedOrigins(List.of("http://localhoast:3000")); // 허용할 경로
+                                        config.setAllowedMethods(List.of("*")); // 허용할 메서드(GET, POST, ...)
+                                        config.setAllowCredentials(true);
+                                        config.setAllowedHeaders(List.of("*")); // 허용할 헤더
+                                        config.setMaxAge(3600L); // 허용할 시간
+                                        config.setExposedHeaders(List.of("Authorization")); // 최종 반환할 헤더
+                                        return config;
+                                    }
+                                })
+                );
+
         // STATELESS Session 방식이기 때문에 CSRF가 필요 없으므로 disable 설정
         http.csrf(AbstractHttpConfigurer::disable);
 
