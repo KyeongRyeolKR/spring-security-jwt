45d6e48181a67c721e21406848f7758ec73cf8c3
김경렬 <rlarudfuf802@naver.com>
2024-03-21 18:59:11 +0900

    chore: 세션 정보 확인

--- src/main/java/com/example/securityjwt/controller/MainController.java
+++ src/main/java/com/example/securityjwt/controller/MainController.java
@@ -1,13 +1,21 @@
 package com.example.securityjwt.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String role = SecurityContextHolder.getContext().getAuthentication().getAuthorities().iterator().next().getAuthority();
+        log.info("username={}", username);
+        log.info("role={}", role);
+
+        return "Main Controller : " + username + " " + role;
     }
 }
